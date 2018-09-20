fdd3e2bf02de31b7d1bdd5ad128dd17c29fad269
Kathleen <kdy3@hawaii.edu>
2018-09-19 17:27:47 -1000

    Improved squared() function

--- squared.js
+++ squared.js
@@ -1,10 +1,6 @@
 
 function squared(array) {
-  const final = [];
-  const perfect = _.groupBy(array, x => Math.sqrt(x) % 1 === 0);
-  final.push(perfect.true);
-  final.push(perfect.false);
-  return final;
+  return _.partition(array, x => Math.sqrt(x) % 1 === 0);
 }
 
 const a1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
